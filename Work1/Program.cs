e1ee0bbc6d2613485768aaa90e69ac567d10d610
Artem <artem.roi@globallogic.com>
2022-11-11 16:52:58 +0200

    added Sea class

---
+++ Work1/Program.cs
@@ -0,0 +1,37 @@
+﻿using SeaBattle;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SeaBattle
+{
+    class Program {
+        static void Main()
+        {
+            BattleShip MyBattleShip = new BattleShip();
+            Sea MySea = new Sea();
+
+            void AddShipToList()
+            {
+                
+                //ShipsAdd;
+
+            }
+        }
+    }
+}
+
+// if (sea.ship is BattleShip)
+// {
+//   BattleShip a = (BattleShip)sea.ship;
+//    a.attack();
+// }
+// else if (sea.ship is ServiceShip)
+// {
+//    ServiceShip b = (ServiceShip)sea.ship;
+//    b.Repair();
+// }
+// else if (sea.ship is HybridShip)
+// {
+//    HybridShip c = (HybridShip)sea.ship;
+//  c.attack && c.repair;
+// }
---
+++ Work1/Ship.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    
+        class BaseShip
+        {
+            int Durability;
+            int MoveSpeed;
+            int MoveDirectionX;
+            int MoveDirectionY;
+        }
+
+
+
+        interface IAttack
+        {
+            void Shoot();
+        }
+        interface IRepair
+        {
+            void Repair();
+        }
+
+        public class BattleShip : IAttack
+        {
+            void IAttack.Shoot() { }
+
+        }
+
+        public class ServiceShip : IRepair
+        {
+            void IRepair.Repair() { }
+
+        }
+
+        public class HybridShip : IAttack, IRepair
+        {
+            void IRepair.Repair() { }
+            void IAttack.Shoot() { }
+        }
+ }
+
+
