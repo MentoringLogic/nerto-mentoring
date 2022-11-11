a315168063e81fde08a74818318efd8e17da2d38
RoiArtem <117904761+RoiArtem@users.noreply.github.com>
2022-11-11 17:12:32 +0200

    Add files via upload

---
+++ Sea.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+        public class Sea
+        {
+            private List<BaseShip> Ships { get; set; }
+            int SeaWidth;
+            int SeaLenght;
+
+            public Sea()
+            {
+                var Ships = new List<BaseShip>();   
+            }
+
+            public  void AddShip()
+            {
+                //Add ;
+            }   
+            public void GetShipByIndex()
+            {
+                //...
+            }
+            public void SetShipByIndex()
+            {
+                //...
+            }
+
+        }
+}
+
