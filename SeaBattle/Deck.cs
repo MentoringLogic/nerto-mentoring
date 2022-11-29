0e1dac00ebd44629b81ffc3343f3237700b169cd
Artem <artem.roi@globallogic.com>
2022-11-29 12:13:17 +0200

    updated Deck

--- SeaBattle/Deck.cs
+++ SeaBattle/Deck.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 namespace SeaBattle
 {
     public class Deck
     {
         public Point Location;
         // State for decks true = Onfloat, false = Sank 
-        public bool State { get; set; } = true;
+        public bool IsAlive { get; set; } = true;
     }
 }
